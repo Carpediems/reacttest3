01e59195762ce93b1633fe8dbc809d0e6d3eed13
gaolongqi <gaolongqi@weigaogroup.com>
2022-03-21 10:35:18 +0800

    更改

---
+++ reacts/src/Applogin.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Applogin extends React.Component{
+    render(){
+        return(
+            <div>
+                <label htmlFor='ids'></label>
+                <input itemID='ids'></input>
+                <button>登录</button>
+            </div>
+        )
+    }
+}
--- reacts/src/index.js
+++ reacts/src/index.js
@@ -4,14 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+function Clock(propes){
+    return(
+        <div>
+            <span>现在是</span>
+            <div>{propes.data.toLocaleTimeString()}</div>
+        </div>
+    )
+}
+function tick(){
+    ReactDOM.render(
+        <Clock data={new Date()}/>,
+        document.getElementById('root')
+    )
+}
+setInterval(tick,1000)
 reportWebVitals();
